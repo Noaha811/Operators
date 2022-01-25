6832c8235b0d3fde48a631339b57b395afccd4ab
Noaha811 <nanderson95@students.mchenry.edu>
2022-01-24 18:05:36 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var a = 100
+    var b = 25
+    var ans = 100 * 25
+    println("Multiplication: " + a + "*" + b + "=" + ans)
+
+    a = 25
+    b = 7
+    ans = a + b
+    println("Addition: " + a + "+" + b + "=" + ans)
+
+    a = 34
+    b = 8
+    ans = a % b
+    println("Modulo: " + a + "%" + b + "=" + ans)
+
+    a = 75
+    b = 3
+    ans = a / b
+    println("Division: " + a + "/" + b + "=" + ans)
+
+    a = 9
+    b = 14
+    ans = a-b
+    println("Subtraction: " + a + "-" + b + "=" + ans)
+}
